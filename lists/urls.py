d1ab1616f40aa71340244b7c4572c9d28c768e46
Kazantcev Stepan <kazantcev.stepan@gmail.com>
2024-10-12 15:26:52 +0500

    Подрезали html проект для отображения списка фильмов.

--- lists/urls.py
+++ lists/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from lists.views import hello_world
+from lists.views import response_check, main_page, adding_movie, movies_list
 
 urlpatterns = [
-    path('', hello_world),
+    path('check', response_check),
+    path('', main_page),
+    path('add', adding_movie),
+    path('list', movies_list),
 ]
--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import json
 
 
 # Create your views here.
 @api_view()
-def hello_world(request):
-    return render(request, 'test.html', context={'hello': 'world'})
+def response_check(request):
+    return Response(data={'message': 'alive'})
+
+
+@api_view()
+def main_page(request):
+    return render(request, 'movie_verse/index.html')
+
+
+@api_view()
+def adding_movie(request):
+    return render(request, 'movie_verse/index.html')
+
+
+@api_view()
+def movies_list(request):
+    with open('templates/movie_verse/db.json', 'r') as f:
+        movies = json.load(f)
+    return Response(movies)
+
