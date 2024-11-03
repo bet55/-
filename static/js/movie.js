cf7a2621c8b968c2f3ba5a05170e94e7ea608035
Stepan Kazancev <stepanda96@yandex.ru>
2024-11-03 16:37:30 +0500

    Модальное окно для оценок.

--- static/js/movie.js
+++ static/js/movie.js
@@ -14,7 +14,10 @@ const cardDuration = document.querySelector('.movie-card p:last-child')
 
 const bookedFilmedStorage = document.querySelector('#booked-films');
 
+
+
 const getMoviesUrl = document.URL + '?format=json'
+
 
 
 const getTimeFromMins = (mins) => {
@@ -58,6 +61,7 @@ const toggleMovieOptions = (target) => {
 const rateMovie = (target) => {
 
     const showRateNoteForm = () => {
+        openModal()
     }
 
     const rateRequest = () => {
@@ -101,9 +105,8 @@ const rateMovie = (target) => {
         return false
     }
 
-    // showRateNoteForm()
+    showRateNoteForm()
     // rateRequest()
-    createNoteElement(movieId)
 
 
     return true
@@ -218,7 +221,6 @@ function changeNotesVisibility() {
         const rateNotes = document.querySelectorAll('.note-container')
         visibility = visibility === 'visible' ? 'hidden' : 'visible';
 
-        console.log(visibility)
         rateNotes.forEach(note => {
             note.style.visibility = visibility
         })
@@ -285,3 +287,162 @@ const btnUp = {
 }
 
 btnUp.addEventListener();
+
+let focusedElementBeforeModal;
+const modal = document.getElementById('modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const openModal = () => {
+  // Save current focus
+  focusedElementBeforeModal = document.activeElement;
+
+  // Listen for and trap the keyboard
+  modal.addEventListener('keydown', trapTabKey);
+
+  // Listen for indicators to close the modal
+  modalOverlay.addEventListener('click', closeModal);
+  // Close btn
+  const closeBtn = document.querySelector('.close-btn');
+  closeBtn.addEventListener('click', closeModal);
+
+  // submit form
+  const form = document.getElementById('review-form');
+  form.addEventListener('submit', submitAddReview, false);
+
+  // Find all focusable children
+  var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+  var focusableElements = modal.querySelectorAll(focusableElementsString);
+  // Convert NodeList to Array
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  var firstTabStop = focusableElements[0];
+  var lastTabStop = focusableElements[focusableElements.length - 1];
+
+  // Show the modal and overlay
+  modal.classList.add('show');
+  modalOverlay.classList.add('show');
+
+  // Focus first child
+  // firstTabStop.focus();
+  const reviewName = document.getElementById('reviewName');
+  reviewName.focus();
+
+  function trapTabKey(e) {
+    // Check for TAB key press
+    if (e.keyCode === 9) {
+
+      // SHIFT + TAB
+      if (e.shiftKey) {
+        if (document.activeElement === firstTabStop) {
+          e.preventDefault();
+          lastTabStop.focus();
+        }
+
+      // TAB
+      } else {
+        if (document.activeElement === lastTabStop) {
+          e.preventDefault();
+          firstTabStop.focus();
+        }
+      }
+    }
+
+    // ESCAPE
+    if (e.keyCode === 27) {
+      closeModal();
+    }
+  }
+};
+
+const submitAddReview = (e) => {
+  // console.log(e);
+  console.log('Form subbmitted!');
+  e.preventDefault();
+  closeModal();
+};
+
+const closeModal = () => {
+  // Hide the modal and overlay
+  modal.classList.remove('show');
+  modalOverlay.classList.remove('show');
+
+  const form = document.getElementById('review-form');
+  form.reset();
+  // Set focus back to element that had it before the modal was opened
+  focusedElementBeforeModal.focus();
+};
+
+const setFocus = (evt) => {
+  const rateRadios = document.getElementsByName('rate');
+  const rateRadiosArr = Array.from(rateRadios);
+  const anyChecked = rateRadiosArr.some(radio => { return radio.checked === true; });
+  // console.log('anyChecked', anyChecked);
+  if (!anyChecked) {
+    const star1 = document.getElementById('star1');
+    star1.focus();
+    // star1.checked = true;
+  }
+};
+
+const navRadioGroup = (evt) => {
+  // console.log('key', evt.key, 'code', evt.code, 'which', evt.which);
+  // console.log(evt);
+
+  const star1 = document.getElementById('star1');
+  const star2 = document.getElementById('star2');
+  const star3 = document.getElementById('star3');
+  const star4 = document.getElementById('star4');
+  const star5 = document.getElementById('star5');
+
+  if (['ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp'].includes(evt.key)) {
+    evt.preventDefault();
+    // console.log('attempting return');
+    if (evt.key === 'ArrowRight' || evt.key === 'ArrowDown') {
+      switch(evt.target.id) {
+        case 'star1':
+          star2.focus();
+          star2.checked = true;
+          break;
+        case 'star2':
+          star3.focus();
+          star3.checked = true;
+          break;
+        case 'star3':
+          star4.focus();
+          star4.checked = true;
+          break;
+        case 'star4':
+          star5.focus();
+          star5.checked = true;
+          break;
+        case 'star5':
+          star1.focus();
+          star1.checked = true;
+          break;
+      }
+    } else if (evt.key === 'ArrowLeft' || evt.key === 'ArrowUp') {
+      switch(evt.target.id) {
+        case 'star1':
+          star5.focus();
+          star5.checked = true;
+          break;
+        case 'star2':
+          star1.focus();
+          star1.checked = true;
+          break;
+        case 'star3':
+          star2.focus();
+          star2.checked = true;
+          break;
+        case 'star4':
+          star3.focus();
+          star3.checked = true;
+          break;
+        case 'star5':
+          star4.focus();
+          star4.checked = true;
+          break;
+      }
+    }
+  }
+};
