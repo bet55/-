437d500c17a8bf5e9d21fd1b952b34bba189fa6f
Stepan Kazancev <stepanda96@yandex.ru>
2024-12-11 14:32:06 +0500

    Кнопка сохранения открытки

--- static/js/postcard.js
+++ static/js/postcard.js
@@ -1,3 +1,22 @@
 import {cartHandler} from "./postcard/movies_cart.js";
 
+
+const savePostcard = () => {
+    const saveButton = document.querySelector('#postcard-save-button');
+    const posters = document.querySelectorAll('.poster');
+    const title = document.querySelector('#invitation-title');
+
+    saveButton.addEventListener('click', e => {
+        title.contentEditable = false;
+
+        const printCSS = '<link href="/static/css/postcard.css" rel="stylesheet" type="text/css">';
+        window.frames["print_frame"].document.body.innerHTML = printCSS + document.querySelector('.postcard-container').innerHTML;
+        window.frames["print_frame"].window.focus();
+        window.frames["print_frame"].window.print();
+    })
+
+
+}
+
+savePostcard();
 cartHandler();
